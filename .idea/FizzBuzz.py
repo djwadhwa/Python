5de6dca74b49c621333c61be1fe9ff290cfa6de3
Stuffdude99 <wadhwadhananjay@gmail.com>
2017-08-29 15:30:36 -0700

    first commit

---
+++ .idea/FizzBuzz.py
@@ -0,0 +1,9 @@
+for num in range(1, 46):
+    if num % 15 == 0:
+        print("fizzbuzz")
+    elif num%3 ==0:
+        print("fizz")
+    elif num%5 == 0:
+        print("buzz")
+    else:
+        print (num)
