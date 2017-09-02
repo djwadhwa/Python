97e659de96d3b8d8ce742cfbfa7fad4ca1460e25
Stuffdude99 <wadhwadhananjay@gmail.com>
2017-09-01 23:42:26 -0700

    added BunnyEars, Recursive Practice

---
+++ BunnyEars.py
@@ -0,0 +1,11 @@
+# We have a number of bunnies and each bunny has two big floppy ears.
+# We want to compute the total number of ears across all
+# the bunnies recursively (without loops or multiplication).
+
+def bunnyEars(bunnies):
+    if (bunnies == 0):
+        return 0;
+    else :
+        return 2+bunnyEars(bunnies-1);
+
+print(bunnyEars(21))
